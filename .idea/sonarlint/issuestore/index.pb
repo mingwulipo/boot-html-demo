164365c16b00566089c5a99c9149e8e7fcbe8020
wuzf <wuzhangfeng@rys.com>
2020-07-16 10:43:05 +0800

    sdf

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+\
+,src/main/java/com/study/DemoApplication.java,4\a\4a3f2b87d159d9d65ce2102ef7a3b689f82629ad
+Q
+!src/main/java/com/study/User.java,6\7\67c8995abc8d29b402ba53119393b4fa4503e24d
+[
++src/main/java/com/study/UserController.java,4\a\4af4c5e6b8b4754bebdcd4f682fe44894c3d2485
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+[
++src/main/java/com/study/PageController.java,f\5\f5d8244079de2fb97aa6623b8628bf2d90159d41
--- src/main/java/com/study/PageController.java
+++ src/main/java/com/study/PageController.java
@@ -1,6 +1,7 @@
 package com.study;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class PageController {
-    @RequestMapping("/hello")
-    public String index(){
-        return "index";
+    @RequestMapping("/goAdd")
+    public String goAdd(Model model){
+        model.addAttribute("name", "lipo");
+        return "add";
     }
 }
